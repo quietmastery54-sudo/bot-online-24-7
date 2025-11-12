2f984625c3770c1dc14fb2fefa57b442dc955486
quietmastery54-sudo <quietmastery54@gmail.com>
2025-11-12 06:27:40 -0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: "minecraft-bot",
+    script: "./bot.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "500M",
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
+    },
+    error_file: "./logs/err.log",
+    out_file: "./logs/out.log",
+    log_file: "./logs/combined.log",
+    time: true
+  }]
+};
